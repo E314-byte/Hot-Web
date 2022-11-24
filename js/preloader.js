daf21f926a00e7293388f3a7aa48dfd1713937b6
Alexander <79362302+E314-byte@users.noreply.github.com>
2022-11-24 21:47:16 +0300

    3.0

---
+++ js/preloader.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    let preloader = document.getElementById('page_preloader');
+    preloader.style.display = 'none';
+}
+
+
+
+// var preloader = document.getElementById('page_preloader');
+
+// document.body.onload = function() {
+//     setTimeout(function() {
+//         if (!preloader.classList.contains('done')) {
+//             preloader.classList.add('done');
+//         }
+//     }, 1000);
+// }
